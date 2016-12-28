3697c057165846fa4759827b817936c0ab99d474
David Hariri <david.hariri@gmail.com>
2016-12-28 10:20:20 -0500

    Sets Up SlackResponse class

--- routes/slack.py
+++ routes/slack.py
@@ -1,5 +1,14 @@
 from flask_restful import Resource, reqparse
 
+class SlackResponse(object):
+	def __init__(self, video_url):
+		self.video_url = video_url
+
+	def to_dict(self):
+		return dict(
+			text=self.video_url
+		)
+
 class SlackRequests(Resource):
 	def post(self):
 		parser = reqparse.RequestParser()
@@ -8,4 +17,6 @@ class SlackRequests(Resource):
 
 		args = parser.parse_args()
 
-		return "Hello World 👋", 200
+		resp = SlackResponse("Hello World")
+
+		return resp.to_dict(), 200
