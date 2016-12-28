675ce7a79e02493b75dc0fd147fc32855a4df598
David Hariri <david.hariri@gmail.com>
2016-12-28 10:56:59 -0500

    Adds YouTube API to providers.youtube

--- providers/youtube.py
+++ providers/youtube.py
@@ -1,7 +1,23 @@
+import os
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
 class Search(object):
 	def __init__(self, text):
 		self.text = text
 		self.results = []
 
 	def execute(self):
-		self.results.append("https://www.youtube.com/watch?v=OVYF4t-v6Zw")
+		youtube = build("youtube", "v3", developerKey=os.environ.get("YOUTUBE_API_KEY"))
+
+		api_response = youtube.search().list(
+			q=self.text,
+			type="video",
+			part="id",
+			maxResults=1
+		).execute()
+
+		# Merge video ids
+		for r in api_response.get("items", []):
+			self.results.append("https://www.youtube.com/watch?v={}".format(r["id"]["videoId"]))
