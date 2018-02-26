07f4e6421763d0e508938c9e97a03baf74d6d09e
David Hariri <david.hariri@gmail.com>
2018-02-26 18:28:56 -0500

    Adds OAuth challnge support for Events API

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 from flask import Flask
 from flask_restful import Api
 from routes.slack import SlackRequests
-from routes.oauth import OAuthRequests
+from routes.oauth import OAuthChallengeRequests
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(SlackRequests, "/")
-api.add_resource(OAuthRequests, "/oauth/")
+api.add_resource(OAuthChallengeRequests, "/oauth/challenge/")
 
 if __name__ == "__main__":
     app.run(debug=os.environ.get("FLASK_DEBUG", False))
--- routes/oauth.py
+++ routes/oauth.py
@@ -1,19 +1,11 @@
 from flask_restful import Resource, reqparse
-from flask import redirect
 
 
-class OAuthRequests(Resource):
-    def get(self):
+class OAuthChallengeRequests(Resource):
+    def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument("text", type=str)
+        parser.add_argument("challenge", type=str)
 
         args = parser.parse_args()
 
-        # Check the token from the requester
-        if args.get("token", "") != os.environ.get("SLACK_VERIFY_TOKEN"):
-            return "Invalid token", 400
-
-        return redirect(
-            "https://github.com/davidhariri/SlackVideo/blob/master/SUCCESS.md",
-            code=302
-        )
+        return args["challenge"], 200
