637d2e79b3d4f0fc776f5714b8ebb8404a3c2902
David Hariri <david.hariri@gmail.com>
2018-02-26 18:10:34 -0500

    Uses pipenv

--- routes/oauth.py
+++ routes/oauth.py
@@ -1,10 +1,19 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import redirect
 
 
 class OAuthRequests(Resource):
     def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("text", type=str)
+
+        args = parser.parse_args()
+
+        # Check the token from the requester
+        if args.get("token", "") != os.environ.get("SLACK_VERIFY_TOKEN"):
+            return "Invalid token", 400
+
         return redirect(
-            "https://github.com/davidhariri/SlackVideo/blob/master/README.md",
+            "https://github.com/davidhariri/SlackVideo/blob/master/SUCCESS.md",
             code=302
         )
